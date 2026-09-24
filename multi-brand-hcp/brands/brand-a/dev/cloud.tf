7f6fdd1c303f926bbae4ae718fea802e15ccb2b9
ben-southpaw <ben.dryden@gmail.com>
2026-09-24 18:10:18 +0200

    Declare the provider in each environment root

--- multi-brand-hcp/brands/brand-a/dev/cloud.tf
+++ multi-brand-hcp/brands/brand-a/dev/cloud.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 1.10"
+
+  required_providers {
+    authsignal = {
+      source  = "authsignal/authsignal"
+      version = "~> 3.10"
+    }
+  }
+
   cloud {
     organization = "authsignal_terraform"
 
